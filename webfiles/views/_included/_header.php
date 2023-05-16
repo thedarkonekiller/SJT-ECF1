dacb758ff91d8c97b04e7fa6cd6fb7436844fac6
Valentin1302 <valentingautier8@gmail.com>
2023-05-16 15:58:46 +0200

    css page pays

--- webfiles/views/_included/_header.php
+++ webfiles/views/_included/_header.php
@@ -32,12 +32,4 @@
    
             </nav>
         </header>
-<main>
-    <div class="whiteBloc">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptas accusantium similique laudantium nostrum, eaque non et aliquid numquam nam, commodi, quam quisquam ullam culpa rerum harum? Totam, asperiores accusamus?
-        Eius asperiores quidem maiores a alias rem tenetur possimus! Id, non animi quis quae libero modi est rem ea enim. Inventore enim ipsam ex accusantium, modi ullam architecto laborum sit?
-        Facere sed quis cumque, officiis voluptas commodi odio. Hic illum odio quisquam laborum ex harum maiores laudantium nobis expedita, modi saepe, esse atque. Quidem laboriosam eos quaerat! Velit, ipsa! Ea?
-        Nihil ipsa harum veritatis dolores, hic tempore provident libero totam pariatur! Delectus corporis sed officiis provident, consequatur ratione veritatis deserunt maxime voluptas labore neque nostrum, iure inventore assumenda autem laboriosam.
-        Voluptatem ipsam numquam debitis voluptates sapiente deleniti perspiciatis adipisci. Totam natus quos, nulla inventore cumque tempore aspernatur aut itaque dignissimos placeat. Delectus molestiae adipisci ipsum temporibus ad dolorem sunt accusantium.</p>
-    </div>
-</main>
+
