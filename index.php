36e4f74a752b8d2cc0f2340dcb19957d1ade390e
thedok02 <florian.charpentier02@gmail.com>
2023-05-16 07:54:35 +0200

    add index.php,_header.php,_footer.php and start header

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/webfiles/views/_included/_header.php') ?>
+
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].'/webfiles/views/_included/_footer.php') ?>
---
+++ webfiles/views/_included/_header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Football Club est un site d'informations sur les clubs de foot mondiaux, vous y trouverez l'historique des clubs, leurs effectifs et des renseignements sur les joueurs">
+    <link rel="stylesheet" href="/webfiles/styles/css/style.css">
+    <title>Football Club</title>
+    <script src="https://kit.fontawesome.com/b94fcc4ff9.js" crossorigin="anonymous"></script>
+</head>
+
+<body class="flex">
+    <main class="flex">
+        <header class="flex">
+            <nav class="flex">
+                <img src="" alt="">
+                <div class="flex flex-col">
+                    <div class="flex">
+                        <a href="">Connexion</a>
+                        <a href="">Inscription</a>
+                        <a href="">Administration</a>
+                        <a href="">Profil</a>
+                        <a href="">Déconnexion</a>
+                    </div>
+                    <ul class="flex">
+                        <li><a href=""></a>Accueil</li>
+                        <li><a href=""></a>Pays</li>
+                        <li><a href=""></a>Ligues</li>
+                        <li><a href=""></a>Clubs</li>
+                        <li><a href=""></a>Joueurs</li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
+    </main>
