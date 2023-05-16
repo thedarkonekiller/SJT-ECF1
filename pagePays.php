4f7018d4c1a62759d354a1a8351b184c92e93cab
Valentin1302 <valentingautier8@gmail.com>
2023-05-16 23:06:29 +0200

    ajout du code sass

--- pagePays.php
+++ pagePays.php
@@ -1,8 +1,9 @@
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_header.php') ?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_footer.php') ?>
-
+<main>
 <h1>Selectionnez votre pays :</h1>
-
+<div class="allCard">
+<div class="firstCard">
 <div class="card">
     <img src="./assets/img/FranceForme.jpg" alt="Forme de la France">
     <h2>France</h2>
@@ -15,6 +16,8 @@
     <img src="./assets/img/carteItalie.png" alt="Forme de l'Italie">
     <h2>Italie</h2>
 </div>
+</div>
+<div class="secondCard">
 <div class="card">
     <img src="./assets/img/cartePortugal.png" alt="Forme du Portugal">
     <h2>Portugal</h2>
@@ -27,3 +30,6 @@
     <img src="./assets/img/carteAllemagne.jpg" alt="Forme de l'Allemagne">
     <h2>Allemagne</h2>
 </div>
+</div>
+</div>
+</main>
