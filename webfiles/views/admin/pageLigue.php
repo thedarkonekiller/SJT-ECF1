926a1fdf274721b2f1eefb4bbd6cc8ebb65c96e5
unknown <alexisgot@hotmail.fr>
2023-05-17 11:05:03 +0200

    Déplacement des pages

---
+++ webfiles/views/admin/pageLigue.php
@@ -0,0 +1,2 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_header.php') ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_footer.php') ?>
---
+++ webfiles/views/admin/pagePays.php
@@ -0,0 +1,35 @@
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_header.php') ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_footer.php') ?>
+<main>
+<h1>Selectionnez votre pays :</h1>
+<div class="allCard">
+<div class="firstCard">
+<div class="card">
+    <img src="./assets/img/FranceForme.jpg" alt="Forme de la France">
+    <h2>France</h2>
+</div>
+<div class="card">
+    <img src="./assets/img/carteEspagne.jpg" alt="Forme de l'Espagne">
+    <h2>Espagne</h2>
+</div>
+<div class="card">
+    <img src="./assets/img/carteItalie.png" alt="Forme de l'Italie">
+    <h2>Italie</h2>
+</div>
+</div>
+<div class="secondCard">
+<div class="card">
+    <img src="./assets/img/cartePortugal.png" alt="Forme du Portugal">
+    <h2>Portugal</h2>
+</div>
+<div class="card">
+    <img src="./assets/img/carteAngleterre.jpg" alt="Forme de l'Angleterre">
+    <h2>Angleterre</h2>
+</div>
+<div class="card">
+    <img src="./assets/img/carteAllemagne.jpg" alt="Forme de l'Allemagne">
+    <h2>Allemagne</h2>
+</div>
+</div>
+</div>
+</main>
