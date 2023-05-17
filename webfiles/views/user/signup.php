5fe34f36879d2907b60596aeeb2fac96ac3e7309
AlexisGot <alexisgot@hotmail.fr>
2023-05-17 15:04:18 +0200

    Correction de bug

--- webfiles/views/user/signup.php
+++ webfiles/views/user/signup.php
@@ -4,14 +4,14 @@ require_once($_SERVER['DOCUMENT_ROOT'].'/webfiles/views/_included/_header.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/webfiles/scripts/user/signupuser.php');
 ?>
 <main class="flex flex-col">
-    <form class="flex flex-col signup" action="" method="POST">
+    <form class="flex flex-col signup" action="/webfiles/scripts/user/signupuser.php" method="POST">
         <h2 class="flex">S'inscrire</h2>
         <input type="text" name="userName" placeholder="Nom d'utilisateur" required />
         <input type="text" name="firstName" placeholder="Prénom" required />
         <input type="text" name="lastName" placeholder="Nom de famille" required />
         <input type="text" name="email" placeholder="Email" required />
         <input type="password" name="password" placeholder="Mot de passe" required />
-        <input type="submit" name="submit" value="S'inscrire" />
+        <button type="submit" name="submit" value="S'inscrire"></button>
         <p>Déjà inscrit?<a href="./login.php"> Connectez-vous ici</a></p>
     </form>
 </main>
