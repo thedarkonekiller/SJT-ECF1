cbc5a0213eda15f53d3b98a12d11ec11bd3a56a9
thedok02 <florian.charpentier02@gmail.com>
2023-05-17 18:46:15 +0200

    maj create

--- webfiles/scripts/admin/create.php
+++ webfiles/scripts/admin/create.php
@@ -105,11 +105,11 @@ if($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['addLeagueName']){
     header('Location: /webfiles/views/admin/league');
 }
 
-function createUser(string $userName, string $lastName, string $firstName, string $email, string $passwd, string $typeUsers = 'usr'){
+function createUser(string $userName, string $lastName, string $firstName, string $email, string $passwd){
 
-    require_once($_SERVER['DOCUMENT_ROOT'].'/webfiles/scripts/admin/dbconnect.php');
+    require($_SERVER['DOCUMENT_ROOT'].'/webfiles/scripts/admin/dbconnect.php');
 
-    $sql = "INSERT INTO user (username, last_name, first_name, email, password, role) VALUES (:userName, :lastName, :firstName, :email, :passwd, :typeUsers)";
+    $sql = "INSERT INTO user (username, lastName, firstName, email, passwd) VALUES (:userName, :lastName, :firstName, :email, :passwd)";
 
     try {
         $req = $conn->prepare($sql);
@@ -118,7 +118,6 @@ function createUser(string $userName, string $lastName, string $firstName, strin
         $req->bindValue(':firstName', $firstName, PDO::PARAM_STR);
         $req->bindValue(':email', $email, PDO::PARAM_STR);
         $req->bindValue(':passwd', $passwd, PDO::PARAM_STR);
-        $req->bindValue(':typeUsers', $typeUsers, PDO::PARAM_STR);
         $req->execute();
 
     }
--- webfiles/scripts/user/signupuser.php
+++ webfiles/scripts/user/signupuser.php
@@ -2,12 +2,16 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/scripts/functions.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/scripts/admin/create.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/scripts/admin/dbConnect.php');
+var_dump($_POST);
 if ($conn){
       if ($_POST) {
             if (empty(Validator($_POST))) {
+                  echo '1';
+
                   if ($_POST['userName'] && $_POST['firstName'] && $_POST['lastName'] && $_POST['email'] && $_POST['password'] && isset($_POST['userName'], $_POST['firstName'], $_POST['lastName'], $_POST['email'], $_POST['password'])) {
+                        echo '2';
                         $userName = $_POST['userName'];
-                        $firstsName = $_POST['firstName'];
+                        $firstName = $_POST['firstName'];
                         $lastName = $_POST['lastName'];
                         $email = $_POST['email'];
                         $password = $_POST['password'];
