9cb3b7e1456e20d922d89598d212cfb2d1d92bfe
AlexisGot <alexisgot@hotmail.fr>
2023-05-17 15:01:31 +0200

    problème merge

--- pageJoueur.php
+++
@@ -1,2 +0,0 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_header.php') ?>
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_footer.php') ?>
