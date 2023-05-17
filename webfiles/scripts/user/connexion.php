b42f5b10c7462865eb2455e008fc2c07e3bc9f68
Valentin1302 <valentingautier8@gmail.com>
2023-05-17 21:00:19 +0200

    script connexion fini

--- webfiles/scripts/user/connexion.php
+++ webfiles/scripts/user/connexion.php
@@ -1,32 +1,50 @@
-<?php 
+<?php session_start(); ?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/scripts/functions.php');
 
+require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/scripts/admin/dbConnect.php');
+if ($conn) {
+    Validator($_POST);
+    // Vérification si le paramètre 'login' est défini dans la requête POST
+    if (isset($_POST['login'])) {
+        $email = $_POST['email'];
+        $passwd = $_POST['password'];
+        $requser = "SELECT * FROM user WHERE email = :email";
+        try {
+            // Préparation de la requête avec un paramètre lié
+            $stmt = $conn->prepare($requser);
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
 
-if(!empty($conn)){
-
-        
-    $email = $_POST["mail"];
-    $password = $_POST["pswrd"];
-
-    $req = "SELECT * FROM users WHERE email='$email' AND passwd='$password'";
-
-    $exec = $conn->query($req);
-
-    if($exec != false){
-        $result = $exec->fetchAll(PDO::FETCH_ASSOC);
-            if(!empty($result)){
-                session_start();
-                $_SESSION["connected"] = TRUE;
-                header('Location: /index.php');
-                
-            }else{
-                echo "Vous avez réussi à vous connecter";?>
-            <?php }
-    
-    }else{
-        echo "Echec de la connexion, vérifier votre adresse mail ou le mot de passe";?>
-    <?php }
-
+            // Récupération du résultat de la requête
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($user) {
+                if (password_verify($passwd, $user['passwd'])) {
+                    // Stockage des informations de l'utilisateur dans la session
+                    $_SESSION["user"] = [
+                        "id" => $user["id"],
+                        "pseudo" => $user["username"],
+                        "email" => $user["email"],
+                        "role" => $user["role"]
+                    ];
+                    // Redirection vers la page du profil utilisateur
+                    RedirectToURL("/webfiles/views/user/profil.php", 0.1);
+                    exit;
+                } else {
+                    echo "Les informations envoyées ne permettent pas de vous identifier";
+                    RedirectToURL("/webfiles/views/user/connexion.php", 3);
+                    exit;
+                }
+            } else {
+                echo "L'utilisateur n'existe pas";
+                RedirectToURL("/webfiles/views/user/connexion.php", 3);
+                exit;
+            }
+        } catch (PDOException $e) {
+            // Capture et gestion des exceptions liées à la base de données
+            echo "Une erreur s'est produite lors de l'exécution de la requête : " . $e->getMessage();
+        }
+    }
 }
-else{
-    echo "La BDD n'est pas connecté";
-}
+
+
+?>
--- webfiles/views/user/connexion.php
+++ webfiles/views/user/connexion.php
@@ -4,11 +4,11 @@
  
     <div class="mailForm">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" name="email">
+        <input type="email" class="form-control" name="mail">
     </div>
     <div class="passForm">
         <label for="password" class="form-label">Mot de passe</label>
-        <input type="password" class="form-control" id="password" name="password">
+        <input type="password" class="form-control" name="pswrd">
     </div>
     <button type="submit" class="sendForm">Envoyer</button>
 </form>
