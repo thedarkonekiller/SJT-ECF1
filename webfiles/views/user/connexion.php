9222182b4714af2eaf51570c35c50f86b4b5a97c
thedok02 <florian.charpentier02@gmail.com>
2023-05-18 07:05:52 +0200

    pb de merge (encore)

---
+++ webfiles/views/user/connexion.php
@@ -0,0 +1,16 @@
+
+
+<form action="/webfiles/scripts/user/connexion.php" method="POST">
+ 
+    <div class="mailForm">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" class="form-control" name="email">
+    </div>
+    <div class="passForm">
+        <label for="password" class="form-label">Mot de passe</label>
+        <input type="password" class="form-control" id="password" name="password">
+    </div>
+    <button type="submit" name="login" class="sendForm">Envoyer</button>
+</form>
+
+
