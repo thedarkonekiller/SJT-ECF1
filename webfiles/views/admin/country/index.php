23e56ea6569ba318cbda3978d292654cc49a6f86
thedok02 <florian.charpentier02@gmail.com>
2023-05-19 07:49:24 +0200

    modification of the action paths in the forms following the recentralisation of the processing in the create.php and delete.php files, addition of the header and footer for the display of the error message of prohibition of access to the backoffice in order to stylize it

--- webfiles/views/admin/country/index.php
+++ webfiles/views/admin/country/index.php
@@ -1,24 +1,9 @@
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/scripts/admin/create.php'); ?>
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/scripts/admin/read.php'); ?>
-<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/scripts/admin/delete.php'); ?>
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['addCountryName']) {
-    $name = $_POST['addCountryName'];
-    createCountry($name);
-    header('Location: /webfiles/views/admin/country');
-} elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['deleteIdCountry']) {
-    $id = $_POST['deleteIdCountry'];
-    deleteCountry($id);
-    header('Location: /webfiles/views/admin/country');
-}
-
-?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_admin_header.php') ?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'] . '/webfiles/views/_included/_admin_aside.php') ?>
 <main>
     <section class="add">
         <h2>Ajouter un pays</h2>
-        <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="flex flex-col">
+        <form action="/webfiles/scripts/admin/create.php" method="POST" class="flex flex-col">
             <label for="countryName">Nom</label>
             <input type="text" name="addCountryName" id="countryName">
             <button type="submit" name="addCountry">Ajouter</button>
@@ -44,7 +29,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['addCountryName']) {
                                 <input type="hidden" name="modifyIdCountry" value="<?php echo $result["id"]; ?>">
                                 <button class="faw" type="submit" name="modifyCountry"><i class="fa-solid fa-pen-to-square"></i></button>
                             </form>
-                            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+                            <form action="/webfiles/scripts/admin/delete.php" method="POST">
                                 <input type="hidden" name="deleteIdCountry" value="<?php echo $result["id"]; ?>">
                                 <button class="faw" type="submit" name="deleteCountry"><i class="fa-solid fa-trash-can-arrow-up"></i></button>
                             </form>
--- webfiles/views/admin/index.php
+++ webfiles/views/admin/index.php
@@ -6,7 +6,9 @@
     } 
     // Si on arrive pas à se connecter en tant qu'administrateur
     else { ?>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/webfiles/views/_included/_admin_header.php') ?>
         <p class="danger">Vous n'avez pas accès à cette page</p>
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].'/webfiles/views/_included/_admin_footer.php') ?>
     <?php }
 ?>
 <?php require_once($_SERVER['DOCUMENT_ROOT'].'/webfiles/views/_included/_admin_footer.php') ?>
